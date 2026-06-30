f314e6af4950d4c092b3fbd78bbb32e5fb4f9abf
Adam <adamrpdiass@hotmail.com>
2026-06-30 12:37:27 -0300

    feat: add react router with initial pages

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,40 @@ import './index.css'
 
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router'
 
-import App from './App.jsx'
+import ErrorPage from './pages/error'
+import HomePage from './pages/home'
+import LoginPage from './pages/login'
+import NotFoundPage from './pages/not-found'
+import SignupPage from './pages/signup'
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: 'login',
+        element: <LoginPage />,
+      },
+      {
+        path: 'signup',
+        element: <SignupPage />,
+      },
+      {
+        path: '*',
+        element: <NotFoundPage />,
+      },
+    ],
+  },
+])
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>
 )
