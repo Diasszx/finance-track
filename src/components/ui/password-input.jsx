d0bdc4fddaf4b070573d7c55f9c7cf7b27f0359a
Adam <adamrpdiass@hotmail.com>
2026-06-30 23:50:53 -0300

    feat: add validation to create account form

--- src/components/ui/password-input.jsx
+++ src/components/ui/password-input.jsx
@@ -1,26 +1,36 @@
 import { EyeIcon, EyeOff } from 'lucide-react'
-import { useState } from 'react'
+import { forwardRef, useState } from 'react'
 
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
-const PasswordInput = ({ placeholder = 'Digite sua senha' }) => {
-  const [passwordIsVisible, setPasswordIsVisible] = useState(false)
-  return (
-    <div className="relative">
-      <Input
-        type={passwordIsVisible ? 'text' : 'password'}
-        placeholder={placeholder}
-      />
-      <Button
-        variant="ghost"
-        size="icon"
-        onClick={() => setPasswordIsVisible((prev) => !prev)}
-        className="absolute bottom-0 right-0 top-0 my-auto mr-1 h-8 w-8 text-muted-foreground"
-      >
-        {passwordIsVisible === true ? <EyeOff /> : <EyeIcon />}
-      </Button>
-    </div>
-  )
-}
+
+const PasswordInput = forwardRef(
+  ({ placeholder = 'Digite sua senha', ...props }, ref) => {
+    const [passwordIsVisible, setPasswordIsVisible] = useState(false)
+
+    return (
+      <div className="relative">
+        <Input
+          ref={ref}
+          type={passwordIsVisible ? 'text' : 'password'}
+          placeholder={placeholder}
+          {...props}
+        />
+
+        <Button
+          type="button"
+          variant="ghost"
+          size="icon"
+          onClick={() => setPasswordIsVisible((prev) => !prev)}
+          className="absolute bottom-0 right-0 top-0 my-auto mr-1 h-8 w-8 text-muted-foreground"
+        >
+          {passwordIsVisible ? <EyeOff /> : <EyeIcon />}
+        </Button>
+      </div>
+    )
+  }
+)
+
+PasswordInput.displayName = 'PasswordInput'
 
 export default PasswordInput
--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -1,4 +1,7 @@
+import { zodResolver } from '@hookform/resolvers/zod'
+import { Controller, useForm } from 'react-hook-form'
 import { Link } from 'react-router'
+import { z } from 'zod'
 
 import { Button } from '@/components/ui/button'
 import {
@@ -10,48 +13,240 @@ import {
   CardTitle,
 } from '@/components/ui/card'
 import { Checkbox } from '@/components/ui/checkbox'
-import { Field } from '@/components/ui/field'
+import {
+  Field,
+  FieldError,
+  FieldGroup,
+  FieldLabel,
+} from '@/components/ui/field'
 import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import PasswordInput from '@/components/ui/password-input'
 
+const signupSchema = z
+  .object({
+    firstName: z.string().trim().min(1, {
+      message: 'O nome é obrigatório.',
+    }),
+    lastName: z.string().trim().min(1, {
+      message: 'O sobrenome é obrigatório.',
+    }),
+    email: z
+      .string()
+      .email({
+        message: 'O email é inválido.',
+      })
+      .trim()
+      .min(1, {
+        message: 'O email é obrigatório.',
+      }),
+    password: z.string().min(6, {
+      message: 'A senha deve ter pelo menos 6 caracteres',
+    }),
+    confirmPassword: z.string().min(1, { message: 'Confirme sua senha.' }),
+    terms: z.boolean().refine((isCheck) => isCheck === true, {
+      message: 'Você precisa aceitar os termos.',
+    }),
+  })
+  .refine(({ password, confirmPassword }) => password === confirmPassword, {
+    message: 'As senhas não coincidem',
+    path: ['confirmPassword'],
+  })
+
 const SignupPage = () => {
+  const form = useForm({
+    resolver: zodResolver(signupSchema),
+    defaultValues: {
+      firstName: '',
+      lastName: '',
+      email: '',
+      password: '',
+      confirmPassword: '',
+      terms: false,
+    },
+  })
+
+  const onSubmit = (data) => {
+    console.log(data)
+  }
+
   return (
     <div className="flex h-screen w-screen flex-col items-center justify-center">
-      <Card className="w-[500px]">
+      <Card className="max-h-[85vh] w-[500px] overflow-y-auto">
         <CardHeader className="items-center">
           <CardTitle>Crie a sua conta</CardTitle>
           <CardDescription>Insira seus dados abaixo.</CardDescription>
         </CardHeader>
-        <CardContent className="space-y-4">
-          <Input placeholder="Digite seu nome" />
-          <Input placeholder="Digite seu sobrenome" />
-          <Input placeholder="Digite seu e-mail" />
-          <PasswordInput />
-          <PasswordInput placeholder="Digite sua senha novamente" />
+        <CardContent>
+          <form id="form-rhf" onSubmit={form.handleSubmit(onSubmit)}>
+            <FieldGroup className="gap-2">
+              <Controller
+                name="firstName"
+                control={form.control}
+                render={({ field, fieldState }) => (
+                  <Field data-invalid={fieldState.invalid}>
+                    <FieldLabel
+                      htmlFor="form-rhf-firstName"
+                      className="text-foreground"
+                    >
+                      Nome
+                    </FieldLabel>
+                    <Input
+                      {...field}
+                      id="form-rhf-firstName"
+                      aria-invalid={fieldState.invalid}
+                      placeholder="Digite seu nome"
+                      autoComplete="off"
+                    />
+                    {fieldState.invalid && (
+                      <FieldError errors={[fieldState.error]} />
+                    )}
+                  </Field>
+                )}
+              />
+              <Controller
+                name="lastName"
+                control={form.control}
+                render={({ field, fieldState }) => (
+                  <Field data-invalid={fieldState.invalid}>
+                    <FieldLabel
+                      htmlFor="form-rhf-lastName"
+                      className="text-foreground"
+                    >
+                      Sobrenome
+                    </FieldLabel>
+                    <Input
+                      {...field}
+                      id="form-rhf-lastName"
+                      aria-invalid={fieldState.invalid}
+                      placeholder="Digite seu sobrenome"
+                      autoComplete="off"
+                    />
+                    {fieldState.invalid && (
+                      <FieldError errors={[fieldState.error]} />
+                    )}
+                  </Field>
+                )}
+              />
+              <Controller
+                name="email"
+                control={form.control}
+                render={({ field, fieldState }) => (
+                  <Field data-invalid={fieldState.invalid}>
+                    <FieldLabel
+                      htmlFor="form-rhf-email"
+                      className="text-foreground"
+                    >
+                      E-mail
+                    </FieldLabel>
+                    <Input
+                      {...field}
+                      id="form-rhf-email"
+                      aria-invalid={fieldState.invalid}
+                      placeholder="Digite seu e-mail"
+                      autoComplete="off"
+                    />
+                    {fieldState.invalid && (
+                      <FieldError errors={[fieldState.error]} />
+                    )}
+                  </Field>
+                )}
+              />
+              <Controller
+                name="password"
+                control={form.control}
+                render={({ field, fieldState }) => (
+                  <Field data-invalid={fieldState.invalid}>
+                    <FieldLabel
+                      htmlFor="form-rhf-password"
+                      className="text-foreground"
+                    >
+                      Senha
+                    </FieldLabel>
+                    <PasswordInput
+                      {...field}
+                      id="form-rhf-password"
+                      aria-invalid={fieldState.invalid}
+                      placeholder="Digite sua senha"
+                      autoComplete="off"
+                    />
+                    {fieldState.invalid && (
+                      <FieldError errors={[fieldState.error]} />
+                    )}
+                  </Field>
+                )}
+              />
+              <Controller
+                name="confirmPassword"
+                control={form.control}
+                render={({ field, fieldState }) => (
+                  <Field data-invalid={fieldState.invalid}>
+                    <FieldLabel
+                      htmlFor="form-rhf-confirmPassword"
+                      className="text-foreground"
+                    >
+                      Confirme a Senha
+                    </FieldLabel>
+                    <PasswordInput
+                      {...field}
+                      id="form-rhf-confirmPassword"
+                      aria-invalid={fieldState.invalid}
+                      placeholder="Digite sua senha novamente"
+                      autoComplete="off"
+                    />
+                    {fieldState.invalid && (
+                      <FieldError errors={[fieldState.error]} />
+                    )}
+                  </Field>
+                )}
+              />
+              <Controller
+                name="terms"
+                control={form.control}
+                render={({ field, fieldState }) => (
+                  <Field data-invalid={fieldState.invalid}>
+                    <div className="flex items-start gap-2">
+                      <Checkbox
+                        id="terms-checkbox"
+                        checked={field.value}
+                        onCheckedChange={field.onChange}
+                        aria-invalid={fieldState.invalid}
+                      />
+
+                      <Label
+                        htmlFor="terms-checkbox"
+                        className="text-xs text-muted-foreground"
+                      >
+                        <span className="opacity-75">
+                          Ao clicar em “Criar conta”, você aceita{' '}
+                        </span>
+                        <a href="#" className="text-white underline">
+                          nosso termo de uso e política de privacidade
+                        </a>
+                      </Label>
+                    </div>
+
+                    {fieldState.invalid && (
+                      <FieldError errors={[fieldState.error]} />
+                    )}
+                  </Field>
+                )}
+              />
+            </FieldGroup>
+          </form>
         </CardContent>
-        <Field orientation="horizontal" className="mb-6 ml-6 w-[330px]">
-          <Checkbox id="terms-checkbox" name="terms-checkbox" />
-          <Label
-            htmlFor="terms-checkbox"
-            className="text-xs text-muted-foreground"
-          >
-            <span className="opacity-75">
-              Ao clicar em “Criar conta”, você aceita{' '}
-            </span>
-            <a href="#" className="text-white underline">
-              nosso termo de uso e política de privacidade
-            </a>
-          </Label>
-        </Field>
         <CardFooter>
-          <Button className="w-full">Fazer login</Button>
+          <Button type="submit" className="w-full" form="form-rhf">
+            Fazer login
+          </Button>
         </CardFooter>
       </Card>
       <div className="flex items-center justify-center">
         <p className="text-center opacity-50">Ainda não possui uma conta?</p>
         <Button variant="link" asChild>
-          <Link to="/login">Faça login</Link>
+          <Link to="/login" className="underline">
+            Faça login
+          </Link>
         </Button>
       </div>
     </div>
