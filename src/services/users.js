c39bcf0d4f17b04844664cb0c82c50ba89d264a8
Adam <adamrpdiass@hotmail.com>
2026-07-05 15:16:58 -0300

    docs: add jsdocs to user service

--- src/services/users.js
+++ src/services/users.js
@@ -2,6 +2,17 @@ import { protectedApiFetch, publicApiFetch } from '@/lib/axios'
 import { getRefreshToken } from '@/lib/token'
 
 export const UserService = {
+  /**
+   * Cria um novo usuário
+   * @param {object} input - Usuário a ser criado.
+   * @param {string} input.firstName - Primeiro nome de usuário.
+   * @param {string} input.lastName - Sobrenome do usuário.
+   * @param {string} input.email - Email do usuário.
+   * @param {string} input.password - Senha do usuário.
+   * @returns {Object} Usuário criado.
+   * @returns {string} response.tokens - Tokens de autenticação.
+
+   */
   signup: async (user) => {
     const response = publicApiFetch('/users', { method: 'POST', data: user })
     return {
@@ -12,7 +23,15 @@ export const UserService = {
       tokens: response.tokens,
     }
   },
+  /**
+   * Cria um novo usuário
+   * @param {object} input - Usuário a ser autenticado.
+   * @param {string} input.email - Email do usuário.
+   * @param {string} input.password - Senha do usuário.
+   * @returns {Object} Usuário autenticado.
+   * @returns {string} response.tokens - Tokens de autenticação.
 
+   */
   login: async (user) => {
     const response = await publicApiFetch('/users/login', {
       method: 'POST',
@@ -26,7 +45,10 @@ export const UserService = {
       tokens: response.tokens,
     }
   },
-
+  /**
+   * Retorna usuário autenticado.
+   * @returns {Object} Usuário autenticado.
+   */
   getMe: async () => {
     const response = await protectedApiFetch('/responses/me', { method: 'GET' })
     return {
