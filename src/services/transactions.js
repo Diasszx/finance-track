19fe76c377b7a48286a2029558cf678cabb83bb9
Adam <adamrpdiass@hotmail.com>
2026-07-08 15:10:01 -0300

    feat: add update transaction service a

--- src/services/transactions.js
+++ src/services/transactions.js
@@ -15,19 +15,46 @@ export const TransactionService = {
   create: async (input) => {
     const response = await protectedApiFetch('/transactions/me', {
       method: 'POST',
-      data: input,
+      data: {
+        name: input.name,
+        type: input.type,
+        date: input.date,
+        amount: input.amount,
+      },
     })
     return response
   },
   /**
    * Retorna as transações do usuário autenticado
-   * @param {object} input
-   * @param {string} input.from - Data inicial (YYYY-MM-DD)
-   * @param {string} input.to - Data final (YYYY-MM-DD)
+   * @param {object} input - Dados do usuário autenticado.
+   * @param {string} input.from - Data inicial (YYYY-MM-DD).
+   * @param {string} input.to - Data final (YYYY-MM-DD).
    */
   getAll: async (input) => {
     const query = queryString.stringify({ from: input.from, to: input.to })
     const response = await protectedApiFetch(`/transactions/me?${query}`)
+    return response
+  },
+  /**
+   * Atualiza uma transação do usuário autenticado
+   * @param {object} input - Dados da transação.
+   * @param {string} input.id - ID da transação.
+   * @param {string} input.name - Nome da transação.
+   * @param {string} input.date - Data da transação (YYYY-MM-DD).
+   * @param {number} input.amount - Valor da transação.
+   * @param {string} input.type - Tipo da transação
+   * @returns {object} Transação atualizada
+   */
+  update: async (input) => {
+    const response = await protectedApiFetch(`/transactions/me/${input.id}`, {
+      method: 'PATCH',
+      data: {
+        name: input.name,
+        type: input.type,
+        date: input.date,
+        amount: input.amount,
+      },
+    })
     return response
   },
 }
